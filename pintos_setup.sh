146dc7e7a27502fde48a2c766e90b569ae462559
Khau Liên Kiệt <84630931+khaukhau@users.noreply.github.com>
2023-01-10 15:00:59 +0900

    Add files via upload

---
+++ pintos_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dirname=${PWD}
+echo $dirname
+sed -i "257 c\        my \$name = find_file ('$dirname/src/threads/build/kernel.bin');" ./src/utils/pintos
+sed -i "362 c\    \$name = find_file ('$dirname/src/threads/build/loader.bin') if !defined \$name;" ./src/utils/Pintos.pm
+echo "export PATH=$dirname/src/utils:\$PATH" >> ~/.bashrc
+source ~/.bashrc
